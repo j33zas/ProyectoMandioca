b94be71082c356fe78bf681b9f76f8e343f82630
Tobilona108 <tobias.lonardi@davinci.edu.ar>
2020-03-17 20:59:22 -0300

    Custom Camera

--- Assets/Game/Scripts/CustomCamera.cs
+++ Assets/Game/Scripts/CustomCamera.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class CustomCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform target;
+    public float smooth = 1f;
+    public Vector3 offset;
+    public bool lookAt;
+    public float shakeAmmount;
+    private float shakeDurationCurrent;
+    public float shakeDuration;
+    private bool activeShake;
+
+    private void Start()
     {
-        
+        shakeDurationCurrent = shakeDuration;
+    }
+    private void LateUpdate()
+    {
+        Vector3 desiredposition = target.position + offset;
+
+        Vector3 smoothedposition = Vector3.Lerp(transform.position, desiredposition, smooth * Time.deltaTime);
+        transform.position = smoothedposition;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            activeShake = true;
+            shakeDurationCurrent = shakeDuration;
+            
+        }
+        if (activeShake)
+        {
+            Shake();
+        }
+        if (lookAt)
+        {
+            transform.LookAt(target);
+        }
+       
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Shake()
     {
-        
+        if (shakeDurationCurrent > 0)
+        {
+            transform.position += Random.insideUnitSphere * shakeAmmount;
+            shakeDurationCurrent -= Time.deltaTime;
+        }
+        else
+        {
+            shakeDurationCurrent = 0;
+            activeShake = false;
+        }
+
     }
 }
