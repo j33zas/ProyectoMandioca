3b14f226b61c342fcf464ec18799ad10efadf938
SantiMinio <santtiago.mino@davinci.edu.ar>
2020-03-17 21:11:46 -0300

    PlayerMovement
    Basic Movement

--- Assets/Game/Scripts/Character/CharacterHead.cs
+++ Assets/Game/Scripts/Character/CharacterHead.cs
@@ -1,26 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public class CharacterHead : MonoBehaviour
 {
-
+    Action<float> MovementHorizontal;
+    Action<float> MovementVertical;
+    [SerializeField]
+    float speed;
     //el head va a recibir los inputs
     //primero pasa por aca y no directamente al movement porque tal vez necesitemos extraer la llamada
     //para visualizarlo con algun feedback visual
 
+    private void Awake()
+    {
+        var move = new CharacterMovement(GetComponent<Rigidbody>(), speed);
+
+        MovementHorizontal += move.LeftHorizontal;
+        MovementVertical += move.LeftVerical;
+    }
+
     //Joystick Izquierdo, Movimiento
     public void LeftHorizontal(float axis)
     {
-        //envio datos
-        //feedback
-        //etc
+        MovementHorizontal(axis);
+
+        Debug.Log(axis);
     }
+
     public void LeftVerical(float axis)
-    { 
-        //envio datos
-        //feedback
-        //etc
+    {
+        MovementVertical(axis);
     }
 
     //Joystick Derecho, Rotacion
--- Assets/Game/Scripts/Character/CharacterMovement.cs
+++ Assets/Game/Scripts/Character/CharacterMovement.cs
@@ -1,15 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class CharacterMovement : MonoBehaviour
+public class CharacterMovement
 {
-    //ahora si, aca aplicamos el movimiento
 
+    Rigidbody _rb;
+    float speed;
+
+    public CharacterMovement(Rigidbody rb, float s)
+    {
+        _rb = rb;
+        speed = s;
+    }
 
     //Joystick Izquierdo, Movimiento
-    public void LeftHorizontal(float axis) { }
-    public void LeftVerical(float axis) { }
+    public void LeftHorizontal(float axis)
+    {
+        float velY = _rb.velocity.y;
+        float velZ = _rb.velocity.z;
+
+        _rb.velocity = new Vector3(axis * speed, velY, velZ);
+
+        Debug.Log(axis);
+    }
+
+    public void LeftVerical(float axis)
+    {
+        float velY = _rb.velocity.y;
+        float velX = _rb.velocity.x;
+
+        _rb.velocity = new Vector3(velX, velY, axis * speed);
+    }
 
     //Joystick Derecho, Rotacion
     public void RightHorizontal(float axis) { }
