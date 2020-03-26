7024bdf7896de8e54a2da67d80b19777b9eaba24
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-03-26 00:09:35 -0300

    Testeo rompiendo codigo en el secondary

--- Assets/Game/Scripts/GitHubTest/ScriptInmaster.cs
+++ Assets/Game/Scripts/GitHubTest/ScriptInmaster.cs
@@ -8,11 +8,29 @@ public class ScriptInmaster : MonoBehaviour
     void Start()
     {
         //esto fue escrito en el master
+
+        //SECONDARY Esto esta escrito en el secondary
+        //esto tambien
     }
 
-    // Update is called once per frame
+    // Update is called once per frame// en el master esto fue borrado
     void Update()
     {
-        
+        //SECONDARY asdasdsad
     }
+
+    void NUEVO_SCRIPT()
+    {
+
+    }
+
+
+    esto es un error hecho a proposito
+        asd
+        sad
+        ads
+        asfwer
+        gdfgr
+        rtete
+        rw
 }
