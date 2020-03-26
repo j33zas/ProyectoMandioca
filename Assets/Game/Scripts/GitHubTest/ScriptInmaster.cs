406aa03a3700197c9c231a4266d44bc51b64bdba
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-03-25 23:56:05 -0300

    Pequeño test de github

---
+++ Assets/Game/Scripts/GitHubTest/ScriptInmaster.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptInmaster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //esto fue escrito en el master
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
