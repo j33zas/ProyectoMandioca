eb9b07eaa56db83651e8bf48c235d66d4ae44efa
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-03-27 14:27:28 -0300

    CombatComponent

---
+++ Assets/Game/Scripts/CombatSystem/DummyCombatComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyCombatComponent : CombatComponent
+{
+    [SerializeField] LayerMask _lm;
+    [SerializeField] float distance;
+    public Transform target;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, (target.position - transform.position), out hit, distance, _lm))
+            {
+                if (hit.collider.GetComponent<EntityBase>())
+                {
+                    EntityBase character = hit.collider.GetComponent<EntityBase>();
+                    callback.Invoke(character);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Enemys/DummyEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DummyEnemy : EnemyBase
+{
+    [SerializeField] CombatComponent combatComponent;
+    [SerializeField] int damage;
+    void Start() => combatComponent.Configure(AttackEntity);
+    public void AttackEntity(EntityBase e) => e.TakeDamage(damage);
+    /////////////////////////////////////////////////////////////////
+    //////  En desuso
+    /////////////////////////////////////////////////////////////////
+    public override void TakeDamage(int dmg) { }
+    protected override void OnFixedUpdate() { }
+    protected override void OnPause() { }
+    protected override void OnResume() { }
+    protected override void OnTurnOff() { }
+    protected override void OnTurnOn() { }
+    protected override void OnUpdateEntity() { }
+}
