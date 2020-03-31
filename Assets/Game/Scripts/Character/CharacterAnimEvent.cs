3bc3b248fe9b0897ddcca93d01e081e02a10cc06
SantiMinio <santtiago.mino@davinci.edu.ar>
2020-03-30 21:53:24 -0300

    CharAnim y EventManager (arreglo)

--- Assets/Game/Scripts/Character/CharacterAnimEvent.cs
+++ Assets/Game/Scripts/Character/CharacterAnimEvent.cs
@@ -18,6 +18,11 @@ public class CharacterAnimEvent : MonoBehaviour
         myeventManager.SubscribeToEvent(s, receiver);
     }
 
+    public void Add_Callback(string s, EventManager.EventReceiverParam receiver)
+    {
+        myeventManager.SubscribeToEvent(s, receiver);
+    }
+
     public void EVENT_Callback(string s)
     {
         myeventManager.TriggerEvent(s);
--- Assets/Game/Scripts/DevelopTools/EventManager.cs
+++ Assets/Game/Scripts/DevelopTools/EventManager.cs
@@ -7,9 +7,24 @@ namespace DevelopTools
 {
     public class EventManager
     {
-        public delegate void EventReceiver(params object[] parameterContainer);
-        private Dictionary<string, EventReceiver> events;
+        public delegate void EventReceiverParam(params object[] parameterContainer);
+        public delegate void EventReceiver();
 
+        private Dictionary<string, EventReceiverParam> eventsParam= new Dictionary<string, EventReceiverParam>();
+
+        private Dictionary<string, EventReceiver> events= new Dictionary<string, EventReceiver>();
+
+
+        public void SubscribeToEvent(string eventType, EventReceiverParam listener)
+        {
+            if (eventsParam == null)
+                eventsParam = new Dictionary<string, EventReceiverParam>();
+
+            if (!eventsParam.ContainsKey(eventType))
+                eventsParam.Add(eventType, null);
+
+            eventsParam[eventType] += listener;
+        }
 
         public void SubscribeToEvent(string eventType, EventReceiver listener)
         {
@@ -20,6 +35,15 @@ namespace DevelopTools
                 events.Add(eventType, null);
 
             events[eventType] += listener;
+        }
+
+        public void UnsubscribeToEvent(string eventType, EventReceiverParam listener)
+        {
+            if (eventsParam != null)
+            {
+                if (eventsParam.ContainsKey(eventType))
+                    eventsParam[eventType] -= listener;
+            }
         }
 
         public void UnsubscribeToEvent(string eventType, EventReceiver listener)
@@ -38,16 +62,22 @@ namespace DevelopTools
 
         public void TriggerEvent(string eventType, params object[] parametersWrapper)
         {
-            if (events == null)
+            if (eventsParam.Count == 0 && events.Count==0)
             {
                 Debug.LogWarning("No events subscribed");
                 return;
             }
 
+            if (eventsParam.ContainsKey(eventType))
+            {
+                if (eventsParam[eventType] != null)
+                    eventsParam[eventType](parametersWrapper);
+            }
+
             if (events.ContainsKey(eventType))
             {
                 if (events[eventType] != null)
-                    events[eventType](parametersWrapper);
+                    events[eventType]();
             }
         }
     }
