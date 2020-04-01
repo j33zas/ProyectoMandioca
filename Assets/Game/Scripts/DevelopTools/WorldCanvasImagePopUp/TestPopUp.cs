970e5194a363d0412f042a50e1989a5f8c1e25dd
Francisco Criscuolo <fran0w@Franciscos-MacBook-Pro.local>
2020-04-01 18:47:32 -0300

    Canvas PopUp Base

---
+++ Assets/Game/Scripts/DevelopTools/WorldCanvasImagePopUp/TestPopUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPopUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            CanvasPopUpInWorld_Manager.instance.MakeNewWorldPopUp(Icon.parry, new Vector3(2, 2, 2));
+        }
+    }
+}
