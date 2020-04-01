53da473a0f9fbcb203301ea195ddad88ca14e30c
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-03-31 23:25:38 -0300

    Update CharacterAnimator.cs

--- Assets/Game/Scripts/Character/CharacterAnimator.cs
+++ Assets/Game/Scripts/Character/CharacterAnimator.cs
@@ -4,48 +4,13 @@ using UnityEngine;
 
 public class CharacterAnimator : BaseAnimator
 {
-    public CharacterAnimator(Animator _anim) : base(_anim)
-    {
-
-    }
-
-    public void Move(float _speed)
-    {
-        myAnim.SetFloat("Speed", _speed);
-    }
-
-    public void Roll()
-    {
-        myAnim.SetTrigger("Roll");
-    }
-
-    public void Block(bool _block)
-    {
-        myAnim.SetBool("BeginBlock", _block);
-    }
-
-    public void BlockSomething()
-    {
-        myAnim.SetTrigger("BlockSomething");
-    }
-
-    public void Parry()
-    {
-        myAnim.SetTrigger("Parry");
-    }
-
-    public void OnAttackBegin()
-    {
-        myAnim.SetTrigger("AttackBegin");
-    }
-
-    public void NormalAttack()
-    {
-        myAnim.SetTrigger("NormalAttack");
-    }
-
-    public void HeavyAttack()
-    {
-        myAnim.SetTrigger("HeavyAttack");
-    }
+    public CharacterAnimator(Animator _anim) : base(_anim) { }
+    public void Move(float _speed) => myAnim.SetFloat("Speed", _speed);
+    public void Roll() => myAnim.SetTrigger("Roll");
+    public void Block(bool _block) => myAnim.SetBool("BeginBlock", _block);
+    public void BlockSomething() => myAnim.SetTrigger("BlockSomething");
+    public void Parry() => myAnim.SetTrigger("Parry");
+    public void OnAttackBegin() => myAnim.SetTrigger("AttackBegin");
+    public void NormalAttack() => myAnim.SetTrigger("NormalAttack");
+    public void HeavyAttack() => myAnim.SetTrigger("HeavyAttack");
 }
