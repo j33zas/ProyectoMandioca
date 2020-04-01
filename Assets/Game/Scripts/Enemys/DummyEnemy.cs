08d58aed3072cb22a604c6f37408b8b11d058e19
Tobilona108 <tobias.lonardi@davinci.edu.ar>
2020-04-01 19:42:22 -0300

    agrege el statemachine al dummy Enemy

--- Assets/Game/Scripts/Enemys/DummyEnemy.cs
+++ Assets/Game/Scripts/Enemys/DummyEnemy.cs
@@ -20,19 +20,29 @@ public class DummyEnemy : EnemyBase
     public float time_stun;
 
     public AnimEvent anim;
-
+    StatesMachine sm;
+    public Animator animator;
+    [SerializeField]
+    private float _speedMovement;
+    [SerializeField]
+    private float _rotSpeed;
     //public Follow follow;
 
     public Rigidbody _rb;
 
     void Start()
     {
+        _rb = GetComponent<Rigidbody>();
         combatComponent.Configure(AttackEntity);
         feedbackStun = new PopSignalFeedback(time_stun, obj_feedbackStun, EndStun);
         feedbackHitShield = new PopSignalFeedback(0.2f, obj_feedbackShield);
         feedbackAttack = new PopSignalFeedback(0.2f, obj_feedbackattack);
 
         anim.Add_Callback("DealDamage", DealDamage);
+        sm = new StatesMachine();
+        sm.Addstate(new StatesFollow(sm, transform, _rb, FindObjectOfType<CharacterHead>().transform, animator, _rotSpeed, _speedMovement));
+        sm.Addstate(new StatesAttack(sm, animator, transform, FindObjectOfType<CharacterHead>().transform, _rotSpeed, _speedMovement));
+        sm.ChangeState<StatesAttack>();
 
         //follow.Configure(_rb);
     }
@@ -57,7 +67,7 @@ public class DummyEnemy : EnemyBase
         }
     }
 
-    private void Update() { feedbackStun.Refresh();  feedbackHitShield.Refresh(); }
+    private void Update() { feedbackStun.Refresh();  feedbackHitShield.Refresh();sm.Update(); }
 
     /////////////////////////////////////////////////////////////////
     //////  En desuso
