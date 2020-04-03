a177cea0c7140bbe63396dba0c12392f26fe4157
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-04-03 17:41:16 -0300

    ManagerPLayObject

---
+++ Assets/Game/Scripts/PlayObjectCore/Core/ManagerPlayObjects.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ManagerPlayObjects : MonoBehaviour
+{
+    private void Awake()
+    {
+        FindObjectsOfType<EntityBase>().ToList().ForEach(x => x.On());
+    }
+}
