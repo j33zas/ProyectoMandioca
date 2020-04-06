723598b76a9eaa60e8b3fcddda70b67974afb75f
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-04-06 01:49:11 -0300

    Thread Request Object

--- Assets/Game/Scripts/DevelopTools/SomeTools/SceneMainManager.cs
+++ Assets/Game/Scripts/DevelopTools/SomeTools/SceneMainManager.cs
@@ -1,21 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
 
 public class SceneMainManager : MonoBehaviour
 {
-    //public void Exit(/*MessageScreen.Scene where, MessageScreen.MessaggeType messagetype, */string scenetogo = "", params string[] messages)
-    //{
-    //    //if (FindObjectOfType<SceneMainBase>())
-    //    //    FindObjectOfType<SceneMainBase>()
-    //    //        .Exit(scenetogo == "" ? where : MessageScreen.Scene.other, messagetype, scenetogo, messages);
-    //}
-    public void Exit(string sceneToGo, params string[] messages)
+    public static SceneMainManager instance;
+    private void Awake() => instance = this;
+
+    public bool autofind;
+
+    List<Action<IEnumerable<PlayObject>>> toload = new List<Action<IEnumerable<PlayObject>>>();
+    [SerializeField] PlayObject[] allentities;
+    [SerializeField] CharacterHead character;
+
+    Dictionary<Type, List<PlayObject>> typedic = new Dictionary<Type, List<PlayObject>>();
+
+    // CharacterHead;
+
+    ThreadRequestObject<PlayObject> req;
+
+    public GenericBar bar;
+
+    void Start()
     {
-        //if (FindObjectOfType<SceneMainBase>())
-        //    FindObjectOfType<SceneMainBase>()
-        //        .Exit(MessageScreen.Scene.other, MessageScreen.MessaggeType.commonMessage, sceneToGo, messages);
+        toload.Add(AddToMainCollection);
+        req = new ThreadRequestObject<PlayObject>(
+            this,
+            bar,
+            toload.ToArray()
+            );
     }
+
+    void AddType(Type type, PlayObject playobject)
+    {
+        if (!typedic.ContainsKey(type))
+        {
+            var list = new List<PlayObject>();
+            list.Add(playobject);
+            typedic.Add(type, list);
+        }
+        else
+        {
+            if (!typedic[type].Contains(playobject))
+            {
+                typedic[type].Add(playobject);
+            }
+        }
+    }
+
+    public List<T> GetListOf<T>() where T : PlayObject
+    {
+        List<T> aux = new List<T>();
+        foreach (var obj in allentities)
+        {
+            if (obj.GetType() == typeof(T))
+            {
+                aux.Add((T)obj);
+            }
+        }
+        return aux;
+    }
+
+    void AddToMainCollection(IEnumerable<PlayObject> col) => allentities = col.ToArray();
+
+    public void OnPlayerDeath() { }
 
     public void PlayerDeath()
     {
