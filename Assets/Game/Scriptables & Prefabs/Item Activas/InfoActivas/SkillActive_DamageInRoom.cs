35a99d7509e331be34b0ab108c77232cb3eccfbd
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-04-10 19:52:56 -0300

    activas, skillinfo con informacion e imagenes place holder

---
+++ Assets/Game/Scriptables & Prefabs/Item Activas/InfoActivas/SkillActive_DamageInRoom.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillActive_DamageInRoom : SkillActivas
+{
+    protected override void OnExecute() { }
+    protected override void OnBeginSkill() { }
+    protected override void OnEndSkill() { }
+    protected override void OnUpdateSkill() { }
+}
