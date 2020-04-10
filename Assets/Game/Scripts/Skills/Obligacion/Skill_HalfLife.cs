6d741b99a983ca30fe8bb9a8690b0cc768b7645b
Tobilona108 <tobias.lonardi@davinci.edu.ar>
2020-04-10 16:50:10 -0300

    Skill Half Life

--- Assets/Game/Scripts/Skills/Obligacion/Skill_HalfLife.cs
+++ Assets/Game/Scripts/Skills/Obligacion/Skill_HalfLife.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skill_HalfLife : MonoBehaviour
+public class Skill_HalfLife : SkillBase
 {
-    // Start is called before the first frame update
-    void Start()
+    CharacterHead character;
+
+    private void Start()
     {
-        
+        character = Main.instance.GetChar();
+    }
+    protected override void OnBeginSkill()
+    {
+        var enemysInRoom = Main.instance.GetRoom().myenemies();
+        for (int i = 0; i < enemysInRoom.Count; i++)
+        {
+            enemysInRoom[i].HalfLife();
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnEndSkill()
     {
-        
+        var enemysInRoom = Main.instance.GetRoom().myenemies();
+        for (int i = 0; i < enemysInRoom.Count; i++)
+        {
+            enemysInRoom[i].Mortal();
+
+        }
     }
+
+    protected override void OnUpdateSkill()
+    {
+
+    }
+
+    
 }
