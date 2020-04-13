c9fb883054b60a0c3f7b030137a7e632aa3eb2b2
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-04-13 05:11:21 -0300

    take damage sobrecarga

--- Assets/Game/Scripts/PlayObjectCore/Entities/Entity_Base/EntityBase.cs
+++ Assets/Game/Scripts/PlayObjectCore/Entities/Entity_Base/EntityBase.cs
@@ -4,4 +4,5 @@ public abstract class EntityBase : PlayObject
     [Header("EntitiyBase")]
     public side_type side_Type;
     public abstract Attack_Result TakeDamage(int dmg, Vector3 attackDir, Damagetype damagetype);
+    public virtual Attack_Result TakeDamage(int dmg, Vector3 attackDir, Damagetype damagetype, EntityBase owner_entity) { return Attack_Result.sucessful; }
 }
