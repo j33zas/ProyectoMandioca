71338465169eb4358b6d438cd75d431b278731b1
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-04-20 23:46:46 -0300

    change mesh random para prototipar rapido

---
+++ Assets/FastChangeMesh.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FastChangeMesh : MonoBehaviour
+{
+    public bool change;
+    public List<Mesh> meshesToChange;
+
+   
+    void Update()
+    {
+        if (change)
+        {
+            change = false;
+            var meshes = GetComponentsInChildren<MeshFilter>();
+
+            for (int i = 0; i < meshes.Length; i++)
+            {
+                meshes[i].mesh = meshesToChange[Random.Range(0,meshesToChange.Count)];
+            }
+        }
+    }
+}
